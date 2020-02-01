65a355fb5543cb9a3ef30d06736c6aa515113ab3
sasha walker <sashawalker31@gmail.com>
2020-02-01 03:00:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -17,6 +17,6 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  array = [4, 5, 6, 7]
+  array = ["blake", "ashley", "scott"]
 
-array[0], array[3] = array[3], array[0]
+array[1], array[2] = array[2], array[1]
