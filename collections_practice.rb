16d6d454f53bff9197559fc53841a439a5550795
sasha walker <sashawalker31@gmail.com>
2020-02-01 03:36:02 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -28,7 +28,29 @@ def reverse_array(array)
 end
 
 def kesha_maker
-  money= Array.new
-  array.each { |money| array([2], "$")}
-  money
+  kesha = []
+  array.each do |word|
+    word_array = word.split ""
+    word_array[2] = "$"
+    kesha << word_array.join
+  end
+  kesha
+end
+
+def find_a(array)
+  array.select{|string| string.start_with?("a")}
+end
+
+def sum_array(integers)
+  integers.inject{|sum, n| sum + n}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |string, index|
+    if index == 1
+      string
+    else
+      string << "s"
+    end
+  end
 end
